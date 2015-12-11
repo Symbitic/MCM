b41e7a6fcb986dae15a65c4950f332946c440540
Symbitic <alex.shaw.as@gmail.com>
2015-12-11 17:52:41 -0600

    Begin writing MUTANT_SETUP_VERSION

--- CMake/mutant_modules/MutantSetupVersion.cmake
+++ CMake/mutant_modules/MutantSetupVersion.cmake
@@ -5,10 +5,16 @@
 # Handle library version information. ::
 #
 #   mutant_setup_version(<version>
-#                     VARIABLE_PREFIX <prefix>
-#                     [SOVERSION <soversion>]
-#                     [VERSION_HEADER <filename>]
-#                     [PACKAGE_VERSION_FILE <filename> [COMPATIBILITY <compat>]] )
+#     VARIABLE_PREFIX <prefix>
+#     [SOVERSION <soversion>]
+#     [PACKAGE_VERSION_FILE <filename>]
+#   )
+#
+# Parse a version string and setup a set of version variables. Optionally, also
+# creates a CMake package version file.
+#
+# .. TODO:: Add more documentation from ECMSetupVersion.cmake
+#
 
 ################################################################################
 # Copyright (c) 2015 Mutant Engine Contributors.
@@ -38,5 +44,49 @@ if(_MUTANT_SETUP_VERSION_DEFINED)
 endif()
 set(_MUTANT_SETUP_VERSION_DEFINED TRUE)
 
-function(MUTANT_SETUP_VERSION)
+# Import ``write_basic_package_version_file()``
+include(${CMAKE_ROOT}/Modules/CMakePackageConfigHelpers.cmake)
+
+function(MUTANT_SETUP_VERSION _version)
+    set(BOOLEAN_ARGS)
+    set(SINGLE_ARGS "VARIABLE_PREFIX;SOVERSION;PACKAGE_VERSION_FILE")
+    set(MULTI_ARGS)
+
+    cmake_parse_arguments(MSV "${BOOLEAN_ARGS}" "${SINGLE_ARGS}" "${MULTI_ARGS}"
+        ${ARGN})
+
+    if(MSV_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unknown keywords given to MUTANT_SETUP_VERSION(): "
+            "\"${MSV_UNPARSED_ARGUMENTS}\"")
+    endif()
+
+    if(CMAKE_VERSION VERSION_LESS 3.0.0)
+        message(FATAL_ERROR
+            "MUTANT_SETUP_VERSION() requires CMake 3.0.0 or later.")
+    endif()
+
+    string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" _major
+        "${_version}")
+    string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+.*" "\\1" _minor
+        "${_version}")
+    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" _patch
+        "${_version}")
+
+    if(MSV_VARIABLE_PREFIX)
+        set(${MSV_VARIABLE_PREFIX}_VERSION "${_version}" PARENT_SCOPE)
+        set(${MSV_VARIABLE_PREFIX}_VERSION_MAJOR ${_major} PARENT_SCOPE)
+        set(${MSV_VARIABLE_PREFIX}_VERSION_MINOR ${_minor} PARENT_SCOPE)
+        set(${MSV_VARIABLE_PREFIX}_VERSION_PATCH ${_patch} PARENT_SCOPE)
+    endif()
+
+    if(MSV_PACKAGE_VERSION_FILE)
+        if(NOT MSV_COMPATIBILITY)
+            set(MSV_COMPATIBILITY AnyNewerVersion)
+        endif()
+        write_basic_package_version_file("${MSV_PACKAGE_VERSION_FILE}"
+            VERSION ${_version} COMPATIBILITY ${MSV_COMPATIBILITY})
+    endif()
+
+
 endfunction()
+
